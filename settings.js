58c2c28a20ee01a162236fb23298101471c4518c
oakoak22 <nudanudaser@gmail.com>
2024-09-22 18:18:45 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,7 @@
+const settings = {
+  ownerName: 'CryRaz', // Ganti dengan nama owner lu
+  botToken: '7842743927:AAFsWGsVFx32JQM3tfgubUROivMwgAIZZAM', // Ganti dengan token bot Telegram lu
+  owners: ['6446832987', '6576625630'], // Ganti ID_PEMILIK_KEDUA dengan user ID pemilik kedua
+};
+
+module.exports = settings;
